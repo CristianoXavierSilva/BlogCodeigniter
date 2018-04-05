5930dfd581d1a8d557b594c14c44b1d5af337d46
Cristiano Xavier Silva <cristiano.anime93@gmail.com>
2018-04-05 11:29:36 -0300

    Modelagem da interface gráfica

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -4,16 +4,17 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Home extends CI_Controller {
 
-    public function index() {
-
-        $dados['mensagem'] = "Olá Mundo!";
-        $this->load->view('welcome_message', $dados);
+    public function __construct() {
+        parent::__construct();
     }
 
-    public function testedb() {
-        
-        $dados['mensagem'] = $this->db->get('postagens')->result();
-        echo '<pre>';
-        print_r($dados);
+        public function index() {
+
+        $this->load->view('frontend/template/html-header');
+        $this->load->view('frontend/template/header');
+        $this->load->view('frontend/home');
+        $this->load->view('frontend/template/aside');
+        $this->load->view('frontend/template/footer');
+        $this->load->view('frontend/template/html-footer');
     }
 }
