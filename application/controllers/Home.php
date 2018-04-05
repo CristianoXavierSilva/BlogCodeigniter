092f4908b43eb8667c76ea15a4d754a1d2ae1f70
Cristiano Xavier Silva <cristiano.anime93@gmail.com>
2018-04-05 10:59:54 -0300

    configurações Iniciais

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -1,11 +1,19 @@
 <?php
+
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Home extends CI_Controller {
 
-	public function index()	{
-            
-                $dados['mensagem'] = "Olá Mundo!";
-		$this->load->view('welcome_message', $dados);
-	}
+    public function index() {
+
+        $dados['mensagem'] = "Olá Mundo!";
+        $this->load->view('welcome_message', $dados);
+    }
+
+    public function testedb() {
+        
+        $dados['mensagem'] = $this->db->get('postagens')->result();
+        echo '<pre>';
+        print_r($dados);
+    }
 }
